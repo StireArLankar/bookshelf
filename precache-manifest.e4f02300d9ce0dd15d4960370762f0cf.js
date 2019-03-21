f579d82e06fe8bad248b3311d25e84a4354e4ea5
StireArLankar <stire.ru@gmail.com>
2019-03-22 00:50:39 +0300

    Updates

---
+++ precache-manifest.e4f02300d9ce0dd15d4960370762f0cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "43a9768079256ac8555c",
+    "url": "/bookshelf/static/js/runtime~main.b0cf84b1.js"
+  },
+  {
+    "revision": "0b1f2b86308d2f52c517",
+    "url": "/bookshelf/static/js/main.a5df30c1.chunk.js"
+  },
+  {
+    "revision": "86aa529ddce5ea1bfa3f",
+    "url": "/bookshelf/static/js/2.98c9586d.chunk.js"
+  },
+  {
+    "revision": "0b1f2b86308d2f52c517",
+    "url": "/bookshelf/static/css/main.376523fa.chunk.css"
+  },
+  {
+    "revision": "f3643dce3b9350676ac859da94c717c3",
+    "url": "/bookshelf/index.html"
+  }
+];
